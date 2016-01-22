55d91be67f92d41916d45c8cef35e5449e7556f8
rkp8000 <rpang.contact@gmail.com>
2016-01-22 13:55:44 -0800

    Move some of the basic network stuff for storing data into a base class.

--- network.py
+++ network.py
@@ -9,7 +9,38 @@ def phi(z):
     return 1 / (1 + np.exp(-z))
 
 
-class RateBasedModel(object):
+class VoltageFiringRateModel(object):
+    """
+    Base class for models where each node has a voltage and a firing rate. The conversion function must be 
+    specified in the child class in a function called rate_from_voltage.
+    """
+    def __init__(self):
+        self._vs = None
+        self.rs = None
+        
+        self.vs_history = []
+        
+        self.store_voltages = False
+        
+    def record_data(self):
+        if self.store_voltages:
+            self.vs_history.append(self.vs)
+    
+    @property
+    def vs(self):
+        return self._vs
+    
+    @vs.setter
+    def vs(self, vs):
+        self._vs = vs
+        self.rs = self.rate_from_voltage(self._vs)
+        
+    @property
+    def rs_history(self):
+        return [self.rate_from_voltage(vs) for vs in self.vs_history]
+
+    
+class RateBasedModel(VoltageFiringRateModel):
     """
     Rate-based network model. Rates are calculate by passing a population's voltage through a sigmoid 
     function parameterized by a threshold and steepness.
@@ -36,12 +67,7 @@ class RateBasedModel(object):
         # set other things to their initial values
         self.noise_level = 0
         
-        self._vs = None
-        self.rs = None
-        
-        self.vs_history = []
-        
-        self.store_voltages = False
+        super(self.__class__, self).__init__()
     
     def step(self, drive=0):
         """
@@ -60,6 +86,69 @@ class RateBasedModel(object):
         Calculate firing rate from voltage.
         """
         return phi(self.gs * (vs - self.v_ths))
+    
+    
+class DiscreteTimeSquareLattice(object):
+    """
+    Simple, Conway's-Game-of-Life-esque network arranged on a square lattice. Each unit's activation is determined by the activations of its nearest neighbors.
+    
+    :param shape: lattice dimensions
+    :param activation: value that a node achieves upon activation
+    :param inactivation: value that a node achieves upon inactivation
+    :param threshold: threshold parameter for activation probability sigmoid
+    :param steepness: steepness parameter for activation probability sigmoid
+    """
+    
+    def __init__(self, shape, activation_strength, inactivation_strength, threshold, steepness, weight_type='nearest_neighbor'):
+        self.shape = shape
+        self.activation_strength = activation_strength
+        self.inactivation_strength = inactivation_strength
+        self.threshold = threshold
+        self.steepness = steepness
+        self.w = self.make_weight_matrix(shape, weight_type)
+        
+        self.n_nodes = np.prod(shape)
+        
+        self.voltages = np.zeros((self.n_nodes,), dtype=float)
+        self.firing_rates = np.zeros((self.nodes,), dtype=float)
+        self.node_inputs = np.zeros((self.n_nodes,), dtype=float)
+        
+        # set other things to their initial values
+        self.noise_level = 0
+        
+        self._vs = None
+        self.rs = None
+        
+        self.vs_history = []
+        
+        self.store_voltages = False
+        
+    def sigmoid(self, x):
+        """Sigmoid function using stored parameters."""
+        return phi(self.steepness * (x - self.threshold))
+    
+    def step(self, scalar_drive=0, matrix_drive=None):
+        """
+        Step forward one instant in time, optionally providing drive to the network.
+        
+        :param drive: drive to network, given as scalar or 1-D array
+        """
+        drive = scalar_drive
+        if matrix_drive is not None:
+            drive += matrix_drive.flatten()
+        
+        self.node_inputs = np.zeros((self.n_nodes,), dtype=float)
+        
+        self.node_inputs += self.w.dot(self.firing_rates)
+    
+    def rate_from_voltage(self, vs):
+        """
+        Calculate firing rate from voltage.
+        """
+        # rectify
+        rs = vs.copy()
+        rs[rs <= 0] = 0
+        return rs
     
     def record_data(self):
         if self.store_voltages:
