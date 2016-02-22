103da531ff069fa30ca229202d7359c24498cb58
rkp8000 <rpang.contact@gmail.com>
2016-02-22 14:42:03 -0800

    Add functions for calculating transition probabilities from weight matrix using softmax function and for calculating path probability given transition matrix.

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@
 Metrics for analyzing networks, etc.
 """
 from __future__ import division, print_function
+import numpy as np
 
 
 def paths_of_length(weights, length, start=None):
@@ -28,3 +29,42 @@ def paths_of_length(weights, length, start=None):
         paths = new_paths
 
     return paths
+
+
+def softmax_prob_from_weights(weights, gain):
+    """
+    Convert a weight matrix to a probabilistic transition matrix using a softmax rule.
+    :param weights: weight matrix (rows are targs, cols are srcs)
+    :param gain: scaling factor in softmax function (higher gain -> lower entropy)
+    :return: transition matrix (rows are targs, cols are srcs), initial probabilities
+    """
+
+    # calculate transition probabilities p
+    p_unnormed = np.exp(gain * weights)
+    p = p_unnormed / p_unnormed.sum(0)
+
+    # define p0 to be stationary distribution, given by principal eigenvector
+    evals, evecs = np.linalg.eig(p)
+    idxs_sorted = evals.argsort()[::-1]
+
+    # normalize
+    p0_unnormed = evecs[:, idxs_sorted[0]]
+    p0 = p0_unnormed / p0_unnormed.sum()
+
+    return p, p0
+
+
+def path_probability(path, p, p0):
+    """
+    Calculate the probability of a given path through a softmax network.
+    :param path: sequence of nodes
+    :param p: transition matrix (NOTE: rows are targs, cols are srcs)
+    :param p0: initial node probability
+    :return: probability of path
+    """
+
+    prob = p0[path[0]]
+    for node_ctr, node in enumerate(path[1:]):
+        prob *= p[node, path[node_ctr]]
+
+    return prob
