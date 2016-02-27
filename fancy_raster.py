a7fe19349642855b3aa70d9708bcbf1d1dd55530
Rich Pang <rpang.contact@gmail.com>
2016-02-26 23:28:01 -0700

    Add plotting function that shows spikes as black circles and drives as surrounding circles.

--- fancy_raster.py
+++ fancy_raster.py
@@ -71,3 +71,24 @@ def by_row(ax, spikes, drives, x_offset=0, y_offset=0):
             ys = np.array([row, row])
 
             ax.plot(xs + x_offset, ys + y_offset, c='r', lw=2*drives[time, row], zorder=0)
+
+
+def by_row_circles(ax, spikes, drives, spike_marker_size=10, drive_marker_base_size=100):
+    """
+    Plot raster plots where each spike is a small dark solid circle and each drive is a surrounding hollow brighter
+    circle with a radius that increases with drive strength.
+    :param ax: axis object
+    :param spikes:
+    :param drives:
+    :param spike_marker_size: how big the spike circle is
+    :param drive_marker_base_size: how big the drive circle is for a drive amplitude of 1
+    """
+
+    spike_times, spike_rows = spikes.nonzero()
+
+    ax.scatter(spike_times, spike_rows, c='k', s=spike_marker_size, zorder=1)
+
+    drive_times, drive_rows = drives.nonzero()
+    drive_strengths = drive_marker_base_size * np.array([drives[t, r] for t, r in zip(drive_times, drive_rows)])
+
+    ax.scatter(drive_times, drive_rows, s=drive_strengths, lw=2, facecolors='none', edgecolors='r', zorder=-1)
