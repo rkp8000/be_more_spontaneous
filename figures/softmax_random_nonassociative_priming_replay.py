f19668d382b31a5e8396e815805a6d7a76aacaa2
rkp8000 <rpang.contact@gmail.com>
2016-03-02 16:46:03 -0800

    Add basic demonstration of consequences of having weak connections.

--- figures/softmax_random_nonassociative_priming_replay.py
+++ figures/softmax_random_nonassociative_priming_replay.py
@@ -9,11 +9,10 @@ import numpy as np
 import sys
 import warnings
 warnings.filterwarnings("ignore")
-
 sys.path.append('/Users/rkp/Dropbox/Repositories/be_more_spontaneous')
 
-
 import fancy_raster
+import network
 
 
 def basic_replay(CONFIG):
@@ -233,3 +232,145 @@ def basic_replay(CONFIG):
     axs[7].set_xlabel('time step')
     axs[7].set_ylabel('active ensemble')
     axs[7].set_title('low gain')
+
+
+def novel_pattern_replay(CONFIG):
+    """
+    Show how a network that has weak connections in addition to its strong connections can learn to replay novel patterns that would not normally arise spontaneously.
+    """
+
+    SEED = CONFIG['SEED']
+
+    LOAD_FILE_NAME = CONFIG['LOAD_FILE_NAME']
+
+    W_WEAK = CONFIG['W_WEAK']
+
+    GAIN = CONFIG['GAIN']
+
+    LINGERING_INPUT_VALUE = CONFIG['LINGERING_INPUT_VALUE']
+    LINGERING_INPUT_TIMESCALE = CONFIG['LINGERING_INPUT_TIMESCALE']
+
+    STRONG_DRIVE_AMPLITUDE = CONFIG['STRONG_DRIVE_AMPLITUDE']
+
+    TRIAL_LENGTH_TRIGGERED_REPLAY = CONFIG['TRIAL_LENGTH_TRIGGERED_REPLAY']
+    RUN_LENGTH = CONFIG['RUN_LENGTH']
+
+    FIG_SIZE = CONFIG['FIG_SIZE']
+
+    np.random.seed(SEED)
+
+    fig = plt.figure(figsize=FIG_SIZE, tight_layout=True)
+    axs = []
+    for row_ctr in range(3):
+        axs.append([fig.add_subplot(4, 3, 3*row_ctr + col_ctr) for col_ctr in range(1, 4)])
+    axs = list(axs)
+    axs.append(fig.add_subplot(4, 1, 4))
+
+    # load old network
+    ntwk_old = np.load(LOAD_FILE_NAME)[0]
+
+    # demonstrate how one cannot use intrinsic plasticity to learn sequence that is made of disjoint paths
+    path = list(ntwk_old.node_0_path_tree[0][:])
+    path[2:] = ntwk_old.node_1_path_tree[0][2:]
+    drives = np.zeros((TRIAL_LENGTH_TRIGGERED_REPLAY, ntwk_old.w.shape[0]), dtype=float)
+    for ctr, node in enumerate(path):
+        drives[ctr, node] = STRONG_DRIVE_AMPLITUDE
+    drives[len(path), path[0]] = STRONG_DRIVE_AMPLITUDE
+
+    for ax in axs[0]:
+        ntwk = deepcopy(ntwk_old)
+        ntwk.store_voltages = True
+
+        for drive in drives:
+            ntwk.step(drive)
+
+        spikes = np.array(ntwk.rs_history)
+
+        fancy_raster.by_row_circles(ax, spikes, drives)
+
+        ax.set_xlim(-1, len(drives))
+        ax.set_ylim(-1, 20)
+        ax.set_xlabel('time step')
+        ax.set_ylabel('active ensemble')
+
+    w = ntwk_old.w.copy()
+
+    # add weak connection to element 2 of node_1 path tree from element 1 of node_0 path tree
+    w[ntwk_old.node_1_path_tree[0][2], ntwk_old.node_0_path_tree[0][1]] = W_WEAK
+
+    # make new base network
+    ntwk_base = network.RecurrentSoftMaxLingeringModel(
+        w, GAIN, LINGERING_INPUT_VALUE, LINGERING_INPUT_TIMESCALE
+    )
+    ntwk_base.node_0 = ntwk_old.node_0
+    ntwk_base.node_0 = ntwk_old.node_1
+    ntwk_base.node_0_path_tree = ntwk_old.node_0_path_tree
+    ntwk_base.node_1_path_tree = ntwk_old.node_1_path_tree
+
+    # demonstrate how weak connections do not substantially affect path probabilities
+    path = list(ntwk_base.node_0_path_tree[0][:])
+    path[2:] = ntwk_base.node_1_path_tree[0][2:]
+    drives = np.zeros((TRIAL_LENGTH_TRIGGERED_REPLAY, ntwk_old.w.shape[0]), dtype=float)
+    drives[0, path[0]] = STRONG_DRIVE_AMPLITUDE
+
+    for ax in axs[1]:
+        ntwk = deepcopy(ntwk_base)
+        ntwk.store_voltages = True
+
+        for drive in drives:
+            ntwk.step(drive)
+
+        spikes = np.array(ntwk.rs_history)
+
+        fancy_raster.by_row_circles(ax, spikes, drives)
+
+        ax.set_xlim(-1, len(drives))
+        ax.set_ylim(-1, 20)
+        ax.set_xlabel('time step')
+        ax.set_ylabel('active ensemble')
+
+    # demonstrate how weak connections allow linking of paths into short term memory
+    path = list(ntwk_base.node_0_path_tree[0][:])
+    path[2:] = ntwk_base.node_1_path_tree[0][2:]
+    drives = np.zeros((TRIAL_LENGTH_TRIGGERED_REPLAY, ntwk_base.w.shape[0]), dtype=float)
+    for ctr, node in enumerate(path):
+        drives[ctr, node] = STRONG_DRIVE_AMPLITUDE
+    drives[len(path), path[0]] = STRONG_DRIVE_AMPLITUDE
+
+    for ax in axs[2]:
+        ntwk = deepcopy(ntwk_base)
+        ntwk.store_voltages = True
+
+        for drive in drives:
+            ntwk.step(drive)
+
+        spikes = np.array(ntwk.rs_history)
+
+        fancy_raster.by_row_circles(ax, spikes, drives)
+
+        ax.set_xlim(-1, len(drives))
+        ax.set_ylim(-1, 20)
+        ax.set_xlabel('time step')
+        ax.set_ylabel('active ensemble')
+
+    path = list(ntwk_base.node_0_path_tree[0][:])
+    path[2:] = ntwk_base.node_1_path_tree[0][2:]
+    drives = np.zeros((RUN_LENGTH, ntwk_base.w.shape[0]), dtype=float)
+    for ctr, node in enumerate(path):
+        drives[ctr, node] = STRONG_DRIVE_AMPLITUDE
+
+    ntwk = deepcopy(ntwk_base)
+    ntwk.store_voltages = True
+    for drive in drives:
+        ntwk.step(drive)
+
+    spikes = np.array(ntwk.rs_history)
+
+    fancy_raster.by_row_circles(axs[3], spikes, drives)
+
+    axs[3].set_xlim(-1, len(drives))
+    axs[3].set_ylim(-1, 40)
+    axs[3].set_xlabel('time step')
+    axs[3].set_ylabel('active ensemble')
+
+    # now demonstrate how pattern-matching computation changes
